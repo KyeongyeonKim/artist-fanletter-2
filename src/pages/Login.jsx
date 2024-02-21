2a7dbd362e3c87f5388b23a3cc7e9c7917c5f4bf
KY <rudyeon@naver.com>
2024-02-22 04:34:48 +0900

    [Feat] 5. 로그인 화면 UI 작업

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,125 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import { login } from "redux/modules/authSlice";
+import styled from "styled-components";
 
 function Login() {
-  return <div>Login</div>;
+  const dispatch = useDispatch();
+  const [isLoginForm, setIsLoginForm] = useState(true);
+  const [userId, setUserId] = useState("");
+  const [password, setPassword] = useState("");
+  const [nickname, setNickname] = useState("");
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    if (isLoginForm) {
+      // 로그인 처리
+      dispatch(login());
+      toast.success("로그인 성공");
+      // alert("로그인성공");
+    } else {
+      // 회원가입 처리
+      setIsLoginForm(true);
+      setUserId("");
+      setPassword("");
+      setNickname("");
+      toast.success("회원가입 성공");
+      // alert("회원가입성공");
+    }
+  };
+
+  return (
+    <>
+      <StDiv>
+        <form onSubmit={onSubmitHandler}>
+          <StH1>{isLoginForm ? "로그인" : "회원가입"}</StH1>
+          <StInput
+            type="text"
+            placeholder="아이디 (4~10글자)"
+            minLength={4}
+            maxLength={10}
+            value={userId}
+            onChange={(e) => setUserId(e.target.value)}
+          />
+          <StInput
+            type="password"
+            placeholder="비밀번호 (4~15글자)"
+            minLength={4}
+            maxLength={15}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {!isLoginForm && (
+            <StInput
+              placeholder="닉네임 (1~10글자)"
+              minLength={1}
+              maxLength={10}
+              value={nickname}
+              onChange={(e) => setNickname(e.target.value)}
+            ></StInput>
+          )}
+          <StBtn>{isLoginForm ? "로그인" : "회원가입"}</StBtn>
+          <ToggleText>
+            <span onClick={() => setIsLoginForm((prev) => !prev)}>
+              {isLoginForm ? "회원가입" : "로그인"}
+            </span>
+          </ToggleText>
+        </form>
+      </StDiv>
+    </>
+  );
 }
 
 export default Login;
+
+const StDiv = styled.div`
+  background-color: #ddd;
+  border-radius: 20px;
+  width: 500px;
+  height: 400px;
+  padding: 50px 0;
+  margin: 50px auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StH1 = styled.h1`
+  margin-bottom: 40px;
+  font-size: 25px;
+`;
+
+const StInput = styled.input`
+  width: 400px;
+  height: 40px;
+  display: block;
+  margin-bottom: 20px;
+  border: none;
+  border-bottom: 1px solid black;
+  outline: none;
+`;
+
+const StBtn = styled.button`
+  width: 400px;
+  height: 40px;
+  display: block;
+  margin-bottom: 20px;
+  border: 3px solid #fff;
+  background-color: #ddd;
+  cursor: pointer;
+`;
+
+const ToggleText = styled.div`
+  text-align: center;
+  width: 100%;
+  margin-top: 24px;
+  & span {
+    color: black;
+    user-select: none;
+    cursor: pointer;
+    &:hover {
+      color: red;
+    }
+  }
+`;
--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,10 +1,12 @@
 import letters from "redux/modules/letterSlice";
 import member from "redux/modules/memberSlice";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import auth from "redux/modules/authSlice";
 
 const rootReducer = combineReducers({
   letters,
   member,
+  auth,
 });
 
 const store = configureStore({
